862e7b47c367adab023373bb1b85246d99a12629
trisharajput2400-ui <trisharajput2400@gmail.com>
2025-09-03 12:24:53 +0530

    Add files via upload

---
+++ trisha_lit.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main(){
+int a,b;
+printf("Enter two numbers\n");
+scanf("%d%d",&a,&b);/*
+if(a>b)
+printf("%d is largest",a);
+else*/
+printf("%d is largest",b);
+}
+
+
+
+
+
+
+
+////
